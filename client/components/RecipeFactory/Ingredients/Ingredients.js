234ef42bc5a12e1ba4f0c46a54529e3a742d3fbb
Tatiana Orejuela <tatianaorezap@gmail.com>
2021-10-09 17:49:15 -0500

    initial styles for /create endpoint

--- client/components/RecipeFactory/Ingredients/Ingredients.js
+++ client/components/RecipeFactory/Ingredients/Ingredients.js
@@ -13,15 +13,15 @@ const Ingredients = () => {
         <div className={styles.ingredients__container}>
             <h1>Create your New Recipe!</h1>
             <form>
-                <label htmlFor="recipeTitle">title for your recipe</label>
-                <input type="text" name="recipeTitle" />
+                <label htmlFor="recipeTitle">Title for your recipe</label>
+                <input className={styles.ingredients__input} type="text" name="recipeTitle" />
 
                 <label htmlFor="Description">Description</label>
-                <textarea type="text" name="Description" />
+                <textarea className={styles.ingredients__inputArea} type="text" name="Description" />
 
-                <label>Recipe for <input type="number" /> sevings</label>
+                <label>Recipe for <input className={styles.ingredients__inputNumber} type="number" /> sevings</label>
 
-                <div>
+                <div className={styles.ingredients__categories}>
                     <Badge>Breakfast</Badge>
                     <Badge>Lunch</Badge>
                     <Badge>Dinner</Badge>
@@ -31,32 +31,35 @@ const Ingredients = () => {
                     <Badge>Miscellaneous</Badge>                    
                 </div>
 
+                <section className={styles.ingredients__addIngredient}>
                 <h2>Add the ingredients:</h2>
                 
-                <input type="number" />
-                <DropdownButton title="Select fraction">
-                    <Dropdown.Item>0</Dropdown.Item>
-                    <Dropdown.Item>1/8</Dropdown.Item>
-                    <Dropdown.Item>1/4</Dropdown.Item>
-                    <Dropdown.Item>1/3</Dropdown.Item>
-                    <Dropdown.Item>1/2</Dropdown.Item>
-                    <Dropdown.Item>2/3</Dropdown.Item>
-                    <Dropdown.Item>3/4</Dropdown.Item>
-                </DropdownButton>
+                <input className={styles.ingredients__inputUnit} type="number" />
+                    <DropdownButton className={styles.ingredients__dropdown} title="1/2">
+                        <Dropdown.Item>0</Dropdown.Item>
+                        <Dropdown.Item>1/8</Dropdown.Item>
+                        <Dropdown.Item>1/4</Dropdown.Item>
+                        <Dropdown.Item>1/3</Dropdown.Item>
+                        <Dropdown.Item>1/2</Dropdown.Item>
+                        <Dropdown.Item>2/3</Dropdown.Item>
+                        <Dropdown.Item>3/4</Dropdown.Item>
+                    </DropdownButton>
 
-                <DropdownButton title="Select measure">
-                    <Dropdown.Item>teaspoon</Dropdown.Item>
-                    <Dropdown.Item>tablespoon</Dropdown.Item>
-                    <Dropdown.Item>cup</Dropdown.Item>
-                    <Dropdown.Item>gallon</Dropdown.Item>
-                    <Dropdown.Item>grams</Dropdown.Item>
-                    <Dropdown.Item>kilograms</Dropdown.Item>
-                    <Dropdown.Item>Ounces</Dropdown.Item>
-                    <Dropdown.Item>Litre</Dropdown.Item>
-                    <Dropdown.Item>None</Dropdown.Item>
-                </DropdownButton>
-
-                <Button>Add New Ingredient</Button>
+                    <DropdownButton className={styles.ingredients__dropdown} title="Cup">
+                        <Dropdown.Item>teaspoon</Dropdown.Item>
+                        <Dropdown.Item>tablespoon</Dropdown.Item>
+                        <Dropdown.Item>cup</Dropdown.Item>
+                        <Dropdown.Item>gallon</Dropdown.Item>
+                        <Dropdown.Item>grams</Dropdown.Item>
+                        <Dropdown.Item>kilograms</Dropdown.Item>
+                        <Dropdown.Item>Ounces</Dropdown.Item>
+                        <Dropdown.Item>Litre</Dropdown.Item>
+                        <Dropdown.Item>None</Dropdown.Item>
+                    </DropdownButton>
+                    <input className={styles.ingredients__inputIngredient} type="text" />
+                    <Button >D</Button>
+                    <Button className={styles.ingredients__add}>Add New Ingredient</Button>
+                </section>
                 <Button>Save</Button>
             </form>
         </div>
