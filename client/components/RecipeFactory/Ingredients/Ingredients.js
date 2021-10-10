ba6c44532cac08a295a2ab59cce83a5a4cd3ed61
Tatiana Orejuela <tatianaorezap@gmail.com>
2021-10-09 21:54:56 -0500

    integrate ingredients form with react-hook-form

--- client/components/RecipeFactory/Ingredients/Ingredients.js
+++ client/components/RecipeFactory/Ingredients/Ingredients.js
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import { Dropdown } from "react-bootstrap";
 import { DropdownButton } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Badge } from "react-bootstrap";
+import { useForm, Controller } from 'react-hook-form';
 
 import styles from "./Ingredients.module.css";
 
 
 const Ingredients = () => {
 
+    const { register, handleSubmit, control } = useForm();
+
+    const fractionOptions= ["0", "1/8", "1/4", "1/3", "1/2", "2/3", "3/4"]
+    const measurementOptions = ["Teaspoon", "Tablespoon", "Cup", "Gallon", "Grams", "Kilograms", "Ounces", "Litres", "None"];
+
+    const fractionSelection = fractionOptions.map((option) => 
+        <Dropdown.Item eventKey={option} key={option}>{option}</Dropdown.Item>
+    );
+
+    const measurementSelection = measurementOptions.map((option) => 
+        <Dropdown.Item eventKey={option} key={option}>{option}</Dropdown.Item>
+    );
+
+    const [fractionDropdownState, setfractionDropdownState] = useState(fractionOptions[0]);
+    const [measurementDropdownState, setMeasurementDropdownState] = useState(measurementOptions[0])
+
+    const onSubmit = (data) => console.log("data", data);
 
     return (
         <div className={styles.ingredients__container}>
             <h1>Create your New Recipe!</h1>
-            <form>
-                <label htmlFor="recipeTitle">Title for your recipe</label>
-                <input className={styles.ingredients__input} type="text" name="recipeTitle" />
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="title">Title for your recipe</label>
+                <input {...register("ingredients.title")} className={styles.ingredients__input} type="text" />
 
                 <label htmlFor="Description">Description</label>
-                <textarea className={styles.ingredients__inputArea} type="text" name="Description" />
+                <textarea {...register("ingredients.description")} className={styles.ingredients__inputArea} type="text" />
 
                 <label>Recipe for <input className={styles.ingredients__inputNumber} type="number" /> sevings</label>
 
@@ -28,39 +48,51 @@ const Ingredients = () => {
                     <Badge>Dessert</Badge>
                     <Badge>Appetizer</Badge>
                     <Badge>Beverage</Badge>
-                    <Badge>Miscellaneous</Badge>                    
+                    <Badge>Miscellaneous</Badge>                   
                 </div>
 
                 <section className={styles.ingredients__addIngredient}>
                 <h2>Add the ingredients:</h2>
-                
-                <input className={styles.ingredients__inputUnit} type="number" />
-                    <DropdownButton className={styles.ingredients__dropdown} title="1/2">
-                        <Dropdown.Item>0</Dropdown.Item>
-                        <Dropdown.Item>1/8</Dropdown.Item>
-                        <Dropdown.Item>1/4</Dropdown.Item>
-                        <Dropdown.Item>1/3</Dropdown.Item>
-                        <Dropdown.Item>1/2</Dropdown.Item>
-                        <Dropdown.Item>2/3</Dropdown.Item>
-                        <Dropdown.Item>3/4</Dropdown.Item>
-                    </DropdownButton>
 
-                    <DropdownButton className={styles.ingredients__dropdown} title="Cup">
-                        <Dropdown.Item>teaspoon</Dropdown.Item>
-                        <Dropdown.Item>tablespoon</Dropdown.Item>
-                        <Dropdown.Item>cup</Dropdown.Item>
-                        <Dropdown.Item>gallon</Dropdown.Item>
-                        <Dropdown.Item>grams</Dropdown.Item>
-                        <Dropdown.Item>kilograms</Dropdown.Item>
-                        <Dropdown.Item>Ounces</Dropdown.Item>
-                        <Dropdown.Item>Litre</Dropdown.Item>
-                        <Dropdown.Item>None</Dropdown.Item>
+                <input {...register(`ingredients.list[0].unit`)} className={styles.ingredients__inputUnit} type="number" />
+
+                <Controller 
+                    control={control}
+                    name={`ingredients.list[0].fraction`}
+                    defaultValue={fractionOptions[0]}
+                    render={({field: {onChange, value}}) => 
+                    <DropdownButton
+                    className={styles.ingredients__dropdown} 
+                    title={value}
+                    onSelect={onChange}
+                    > 
+                        {fractionSelection}
                     </DropdownButton>
-                    <input className={styles.ingredients__inputIngredient} type="text" />
+                    }
+                />      
+
+                <Controller 
+                    control={control}
+                    name={`ingredients.list[0].measurement`}
+                    defaultValue={measurementOptions[0]}
+                    render={({field: {onChange, value}}) => 
+                    <DropdownButton
+                    className={styles.ingredients__dropdown} 
+                    title={measurementDropdownState}
+                    onSelect={onChange}
+                    title={value}
+                    > 
+                        {measurementSelection}
+                    </DropdownButton>
+                    }
+                />
+
+                    <input  {...register("ingredients.list[0].name")} className={styles.ingredients__inputIngredient} type="text" />
                     <Button >D</Button>
+
                     <Button className={styles.ingredients__add}>Add New Ingredient</Button>
                 </section>
-                <Button>Save</Button>
+                <Button type="submit" >Save</Button>
             </form>
         </div>
   );
